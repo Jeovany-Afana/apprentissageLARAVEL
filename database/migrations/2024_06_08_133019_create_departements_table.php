7ea874505c2e021d62c63a40b44d90cf62a83d3e
AFANA JEOVANY <wilfrieddylan451@gmail.com>
2024-06-08 13:45:39 +0000

    Connexion du projet à la base de données

--- database/migrations/2024_06_08_133019_create_departements_table.php
+++ database/migrations/2024_06_08_133019_create_departements_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('departements', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('telephone');
-            $table->enum('role',['super admin','admin', 'etudiant'])->default('etudiant');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('departements');
     }
 };
